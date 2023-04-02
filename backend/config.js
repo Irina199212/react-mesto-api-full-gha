551fa090ac38f0f78df640b2f6959ab2ca8c7726
Irina <irinawork1992@yandex.ru>
2023-04-02 17:17:22 +0300

    upd

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 const JWT_SECRET = 'SECRET';
 const PORT = '3000';
-const DB = 'mongodb://localhost:27017/mestodb';
+const DB = 'mongodb://127.0.0.1:27017/mestodb';
 
 module.exports = {
   JWT_SECRET,
