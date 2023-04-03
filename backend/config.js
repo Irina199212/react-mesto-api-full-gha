263dfce085ad9ee56cff3c964698d498c426fa63
Irina <irinawork1992@yandex.ru>
2023-04-04 00:21:32 +0300

    fix

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,7 @@
-const JWT_SECRET = 'SECRET';
-const PORT = '3000';
+require('dotenv').config();
+
+const { JWT_SECRET = 'SECRET' } = process.env;
+const { PORT = '3000' } = process.env;
 const DB = 'mongodb://127.0.0.1:27017/mestodb';
 
 module.exports = {
